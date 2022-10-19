751f6ca3fe50fa52780cd1f720ded35594de8c01
odion100 <odionedwards@gmail.com>
2022-10-19 07:37:40 -0400

    chore(package): updating npm version

--- systemlynx/App/components/loadModules.js
+++ systemlynx/App/components/loadModules.js
@@ -1,5 +1,3 @@
-const Dispatcher = require("../../Dispatcher/Dispatcher");
-
 module.exports = async function loadModules(system, App) {
   system.Modules.forEach(
     (mod) => (mod.module = system.Service.module(mod.name, mod.__constructor))
